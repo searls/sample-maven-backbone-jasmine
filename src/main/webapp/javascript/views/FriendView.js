d94a21660126e01298338ab169c5518520a76145
Olivier Nguyen <olivierntk@gmail.com>
2011-08-18 15:04:16 -0700

    Changes in the pom.xml

--- src/main/webapp/javascript/views/FriendView.js
+++ src/main/webapp/javascript/views/FriendView.js
@@ -1,8 +1,8 @@
 FriendView = Backbone.View.extend({
 	tagName: "li",
 
-	// template: _.template($("#friend-template").html()),
-	template: _.template("<%= name %>"),
+	template: _.template($("#friend-template").html()),
+	// template: _.template("<%= name %>"),
 	
 	render: function () {
 		$(this.el).html(this.template(this.model.toJSON()));
--- src/test/javascript/app1Spec.js
+++ src/test/javascript/app1Spec.js
@@ -1,7 +1,8 @@
 describe("App1: Creating friends", function () {
 	it("should add new friends", function () {
 		loadFixtures('app1.html');
-		var friendApp = new AppView();
+		var f = new Friend;
+		//var friendApp = new AppView;
 		$("#first-name").val("tim");
 		$("form").submit();
 		expect($("#friends-list li").length).toEqual(1);
