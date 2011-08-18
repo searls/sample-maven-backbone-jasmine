70ffbee282633306e8224d7f570f167b98ab6a59
Olivier Nguyen <olivierntk@gmail.com>
2011-08-18 13:32:43 -0700

    Organizing JS files into proper folders

--- src/test/javascript/app1Spec.js
+++ src/test/javascript/app1Spec.js
@@ -1,6 +1,9 @@
 describe("App1: Creating friends", function () {
 	it("should add new friends", function () {
 		loadFixtures('app1.html');
-		var f = new Friend();
+		var friendApp = new AppView();
+		$("#first-name").val("tim");
+		$("form").submit();
+		expect($("#friends-list li").length).toEqual(1);
 	});
 });
